77faccf2e111717681e8989b1e46981c305d3968
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-05-10 20:27:16 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,140 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        {
+            {
+                {
+                    {
+                        {
+                            {
+                                {
+
+
+                                } /* 1 question
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        }
+    } */
+
+                            }/* 2 question
+ public static void main(String[] args) {
+        String M="Mayar" + " " +"Sultan";
+        System.out.println(M);
+
+
+        }
+    } */
+
+                        }/* 3 question
+ public static void main(String[] args) {
+
+        int x=15;
+        int y=10;
+        int a=20;
+        int sum=x+y+a;
+        int average=sum/3;
+        System.out.println("Sum= " +sum);
+        System.out.println("Average= " +average);
+        }
+    }*/
+
+                    }/* 4 question
+
+    public static void main(String[] args) {
+        int num1,num2;
+        num1=5;
+        num2=10;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Please enter the operator (+,-,*,/)");
+        char op=s.next().charAt(0);
+        int M=0;
+        switch (op){
+            case '+':
+                M= num1+num2;
+                break;
+            case '-':
+                M=num1-num2;
+                break;
+            case '*':
+                M=num1*num2;
+                break;
+            case '/':
+                M=num1/num2;
+                break;
+            default:
+                System.out.println("you enter wrong input");
+        }
+        System.out.println("The final result: ");
+        System.out.println(num1+" "+ op +" "+num2+" = "+M);
+
+
+        }
+    } */
+
+                } /* 5 question
+  public static void main(String[] args) {
+
+        int x=15;
+        int y=10;
+        int a=20;
+        int sum=x+y+a;
+        int average=sum/3;
+        System.out.println("Sum= " +sum);
+        System.out.println("Average= " +average);
+        }
+    }*/
+
+
+            }/* 6 question
+
+public static void main(String[] args) {
+        int array[] = {20, 1, 9, 13, 4, 6, 8, 17};
+        int M = array.length;
+        NUM(array);
+
+        System.out.println(array);
+    }
+
+    static void NUM(int array[], int M) {
+        int a=0;
+        for (int i = 0; i < M; i++) {
+            if (array[i] % 2 != 0) {
+                i++;
+                a = array[i];
+                array[i] = array[a];
+                array[a] = a;
+            }
+        }
+    }
+} */
+
+        } /* 7 question
+public static void main (String[]args)
+            throws IOException
+            {
+            String str ="BufferedOutput";
+            str =str.toLowerCase();
+            int count=0;
+            for (int i=0; i<str.length(); i++){
+                if (str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i) == 'i' ||str.charAt(i) == 'o' || str.charAt(i)== 'u') {
+                    count++;
+                }
+            }
+
+            System.out.println("Toal number of vowls in String are: " +count);
+
+            }
+        }*/
+
+    }/* 8 question
+     public static void main(String[] args) {
+        String str=" Mayar@Sultan@Alanaze";
+
+        String[] arrOfStr=str.split("@");
+
+        }
+    }*/
+}
+
